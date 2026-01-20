c9330eefcf3f4a3c1bdd2b7ce5260d13e0a63ee5
dua225 <duafatimaejaz2@gmail.com>
2026-01-20 14:28:46 +0500

    update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -18,56 +18,35 @@ public class MainMenu {
         // Create order
         OrderCart myOrder = new OrderCart();
 
-        System.out.println("Welcome to My Coffee Shop");
+        System.out.println(" Welcome to My Coffee Shop ");
 
         boolean ordering = true;
         while (ordering) {
             // Display menu
-            System.out.println("\n--- Menu ---");
+            System.out.println("\n--- Menu: ---");
             for (int i = 0; i < menu.length; i++) {
-                Coffee c = menu[i];
-                System.out.println((i + 1) + ". " + c.getName() + " - Rs(S/M/L): "
-                        + (int) c.getPrice(Coffee.Size.SMALL) + "/"
-                        + (int) c.getPrice(Coffee.Size.MEDIUM) + "/"
-                        + (int) c.getPrice(Coffee.Size.LARGE));
+                System.out.println((i+1) + ". " + menu[i].getName() + " - Rs: " + menu[i].getPrice());
             }
 
-            System.out.print("Enter your choice (1-" + menu.length + "): ");
+            System.out.print("Enter your choice: ");
             int choice = sc.nextInt();
-            if (choice < 1 || choice > menu.length) {
-                System.out.println("Invalid choice. Please try again.");
+            if(choice < 1 || choice > menu.length){
+                System.out.println("Invalid choice,Please try again!");
                 continue;
             }
 
             System.out.print("Enter quantity: ");
             int qty = sc.nextInt();
-            if (qty <= 0) {
-                System.out.println("Quantity must be at least 1. Please try again.");
-                continue;
-            }
 
-            System.out.println("Choose size: 1. Small  2. Medium  3. Large");
-            int sizeChoice = sc.nextInt();
-            Coffee.Size chosenSize;
-            switch (sizeChoice) {
-                case 2:
-                    chosenSize = Coffee.Size.MEDIUM;
-                    break;
-                case 3:
-                    chosenSize = Coffee.Size.LARGE;
-                    break;
-                default:
-                    chosenSize = Coffee.Size.SMALL;
-                    break;
-            }
+            // Add to order
+            myOrder.addItem(menu[choice-1], qty);
 
-            myOrder.addItem(menu[choice - 1], chosenSize, qty);
+           System.out.print("Do you want to order more items? (yes/no): ");
+           String more = sc.next();  // reads whole word
+           if(more.equalsIgnoreCase("no")){
+        ordering = false;
+}
 
-            System.out.print("Do you want to order more items? (yes/no): ");
-            String more = sc.next();
-            if (more.equalsIgnoreCase("no")) {
-                ordering = false;
-            }
         }
 
         // Show final bill
